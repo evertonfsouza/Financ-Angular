e68f7f865bf1cb5d15cd3f3d0db5052169bad8db
Everton Souza <everton@campussolucoes.com>
2021-06-07 21:53:59 -0300

    Segundo comit

--- ControleFinanceiro/ControleFinanceiro-UI/src/app/app.module.ts
+++ ControleFinanceiro/ControleFinanceiro-UI/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TiposService } from '../app/services/tipos.service';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    TiposService,
+    HttpClientModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
